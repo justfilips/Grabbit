0d8da0fb4555050fc1b377ea3af20441513a4d44
Filips Vaicis <filips.vaicis88@gmail.com>
2025-06-08 16:39:35 +0300

    Impliment item adding in database

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
-use Illuminate\Http\Request;
 use App\Models\Item;
+use App\Models\Category;
+use App\Models\ItemImage;
+use App\Models\ImageImage;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 
 class ItemController extends Controller
@@ -40,7 +43,7 @@ class ItemController extends Controller
             'category_id' => 'required|exists:categories,id',
         ]);
 
-        Item::create([
+        $item = Item::create([
             'title' => $validated['title'],
             'description' => $validated['description'],
             'price' => $validated['price'],
@@ -48,7 +51,18 @@ class ItemController extends Controller
             'category_id' => $validated['category_id'],
             'user_id' => 1, // or set to a fixed ID for now like 1
             'status' => 'pending', // default value
+            'user_id' => Auth::id(),
         ]);
+
+        if ($request->hasFile('image_path')) {
+            $path = $request->file('image_path')->store('item_images', 'public');
+
+            ItemImage::create([
+                'item_id' => $item->id,
+                'image_path' => $path,
+            ]);
+        }
+
 
         return redirect()->route('home')->with('success', 'Item created successfully!');
     }
